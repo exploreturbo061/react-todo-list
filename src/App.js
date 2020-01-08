ea4023306fd6d8601d43d5b030bf00774c37eab4
Sihyun Kim <legitcode267@gmail.com>
2020-01-08 09:39:28 -0700

    Add handleInsert

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
-import React, { Component } from 'react';
-import PageTemplate from './components/PageTemplate';
-import TodoInput from './components/TodoInput';
-import TodoList from './components/TodoList';
+import React, { Component } from "react";
+import PageTemplate from "./components/PageTemplate";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 class App extends Component {
-
   state = {
-    input: '',
+    input: "",
     todos: [
-      { id: 0, text: '리액트 공부하기', done: true },
-      { id: 1, text: '컴포넌트 스타일링 공부 해보기', done: false }
+      { id: 0, text: "리액트 공부하기", done: true },
+      { id: 1, text: "컴포넌트 스타일링 공부 해보기", done: false }
     ]
-  }
+  };
 
-  handleChange = (e) => {
+  id = 1;
+  getId = () => {
+    return ++this.id;
+  };
+
+  handleChange = e => {
     const { value } = e.target;
     this.setState({
       input: value
     });
-  }
+  };
+
+  handleInsert = () => {
+    const { todos, input } = this.state;
+
+    const newTodo = {
+      text: input,
+      done: false,
+      id: this.getId()
+    };
+
+    this.setState({
+      todos: [...todos, newTodo],
+      input: ''
+    });
+  };
 
   render() {
-
     const { input, todos } = this.state;
-    const { 
-      handleChange
-    } = this;
+    const { handleChange, handleInsert } = this;
 
     return (
       <PageTemplate>
-        <TodoInput onChange={handleChange} value={input} />
+        <TodoInput onChange={handleChange} onInsert={handleInsert} value={input} />
         <TodoList todos={todos} />
       </PageTemplate>
-    )
+    );
   }
 }
 
