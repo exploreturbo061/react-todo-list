c22b4a7b55a1aa76405969b25b23c747f40be50f
Sihyun Kim <legitcode267@gmail.com>
2020-01-08 10:18:32 -0700

    Updates

---
+++ precache-manifest.e04188a09f62136a22c2bd2a9c697507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a01d6590dd06eb850a89207210165a1",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "8082fe20a80cd78a80be",
+    "url": "/react-todo-list/static/css/main.c3b457fb.chunk.css"
+  },
+  {
+    "revision": "77d4cb5468fe84b0ed9f",
+    "url": "/react-todo-list/static/js/2.e8fc6c38.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-todo-list/static/js/2.e8fc6c38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8082fe20a80cd78a80be",
+    "url": "/react-todo-list/static/js/main.ca60829c.chunk.js"
+  },
+  {
+    "revision": "659f8c9696ea49a984a4",
+    "url": "/react-todo-list/static/js/runtime-main.33ad9dc7.js"
+  }
+]);
